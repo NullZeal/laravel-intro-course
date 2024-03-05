475b336eb3d784d2fb382e5c39205adb65605954
Julien Pontbriand <nullzeal@pm.me>
2024-03-04 22:25:53 -0500

    Cleared ep.28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,52 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user1 = User::factory()->create();
-
-        $category1 = Category::create([
-            'name'=> 'Family',
-            'slug'=> 'family'
+        $user = User::factory()->create([
+            "name"=> "Julien Pon Pon",
         ]);
 
-        $category2 = Category::create([
-            'name'=> 'Personal',
-            'slug'=> 'personal'
+        Post::factory(5)->create([
+            "user_id"=> $user->id,
         ]);
 
-        $category3 = Category::create([
-            'name'=> 'Work',
-            'slug'=> 'work'
-        ]);
-
-        $post1 = Post::create([
-            'user_id'=> $user1->id,
-            'category_id'=> $category1->id,
-            'title'=> 'My dear sister',
-            'slug' => 'my-first-post',
-            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
-            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
-        ]);
-
-        $post2 = Post::create([
-            'user_id'=> $user1->id,
-            'category_id'=> $category2->id,
-            'title'=> 'My self love for rocks',
-            'slug' => 'my-second-post',
-            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
-            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
-        ]); 
-
-        $post3 = Post::create([
-            'user_id'=> $user1->id,
-            'category_id'=> $category3->id,
-            'title'=> 'My laravel app',
-            'slug' => 'my-third-post',
-            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
-            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
-        ]);
+        Post::factory(5)->create();
     }
 }
