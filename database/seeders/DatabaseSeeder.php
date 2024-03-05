45c631a38dd2a7ce4f8011f3d9dd64e79c08049c
Julien Pontbriand <nullzeal@pm.me>
2024-03-04 21:43:56 -0500

    Cleared ep.27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create();
+
+        $category1 = Category::create([
+            'name'=> 'Family',
+            'slug'=> 'family'
+        ]);
+
+        $category2 = Category::create([
+            'name'=> 'Personal',
+            'slug'=> 'personal'
+        ]);
+
+        $category3 = Category::create([
+            'name'=> 'Work',
+            'slug'=> 'work'
+        ]);
+
+        $post1 = Post::create([
+            'user_id'=> $user1->id,
+            'category_id'=> $category1->id,
+            'title'=> 'My dear sister',
+            'slug' => 'my-first-post',
+            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
+            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
+        ]);
+
+        $post2 = Post::create([
+            'user_id'=> $user1->id,
+            'category_id'=> $category2->id,
+            'title'=> 'My self love for rocks',
+            'slug' => 'my-second-post',
+            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
+            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
+        ]); 
+
+        $post3 = Post::create([
+            'user_id'=> $user1->id,
+            'category_id'=> $category3->id,
+            'title'=> 'My laravel app',
+            'slug' => 'my-third-post',
+            'excerpt'=> '<p>Lorem ipsum dolar sit amet.<p>',
+            'body'=> 'Lorem ipsum blablabla sit amet consectereur adipscing elit.',
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,12 +3,16 @@
             <h2>
                 {!! $post->title !!}
             </h2>
+
+            <p>
+                By <a href="{{ $post->user->name }}">Julien P</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 {!! $post->body !!}
             </div>
 
         </article>
 
-        <p>That was the article.</p>
         <a href="/">Go back</a>
 </x-layout>
