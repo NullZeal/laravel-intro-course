21ed2691c9f26698adc3a6daaf8533228db9c6c2
Julien Pontbriand <nullzeal@pm.me>
2024-03-09 23:10:01 -0500

    Cleared ep.40

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class,'show'])->name('show');
+
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
-        'posts' => $author->posts,
-        'categories'=> Category::all()
+        'posts' => $author->posts
     ]);
 
 });
