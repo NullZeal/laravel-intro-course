4d1cfb6a0f37bed595ddffdc21cf7b9861bc422c
Julien Pontbriand <nullzeal@pm.me>
2024-03-10 15:41:25 -0400

    Cleared ep.42

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -13,13 +13,20 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center lg:w-42 bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
-                <input 
+            <form method="GET" action="/">
+            
+            @if (request('category'))
+            <input type="hidden" name="category" value="{{ request('category') }} ">
+            @endif
+            
+            <input 
                     type="text" 
                     name="search" 
                     placeholder="Find something!"
                     class="bg-transparent placeholder-black font-semibold text-sm"
-                    value="{{ request('search') }}">
+                    value="{{ request('search') }}"
+                    >
+
             </form>
         </div>
 
