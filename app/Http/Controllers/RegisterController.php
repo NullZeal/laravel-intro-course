30d11117b92fe785cb610ce9be00937a5b21cd5f
Julien Pontbriand <nullzeal@pm.me>
2024-03-11 09:52:18 -0400

    Cleared ep.49

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,7 +23,9 @@ class RegisterController extends Controller
             'password'=> ['required', 'max:255', 'min:7'],
             ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        auth()->login($user);
 
         return redirect()->route('home')->with('success','Account created!');;
     }
