42b5cae7bc0f60e2e3046734d960168c21ab7e3c
Julien Pontbriand <nullzeal@pm.me>
2024-03-11 18:40:00 -0400

    Cleared ep.51

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,17 +27,13 @@ class SessionsController extends Controller
             "password"=> "required"
         ]);
 
-        if (auth()->attempt($attributes)){
-            session()->regenerate();
-            return redirect("/")->with("success","Welcome back!");
+        if (! auth()->attempt($attributes)){
+            throw ValidationValidationException::withMessages([
+                "email"=> "Your provided credentials could not be verified!"
+            ]);
         }
 
-        throw ValidationValidationException::withMessages([
-            "email"=> "Your provided credentials could not be verified!"
-        ]);
-
-        // return back()
-        // ->withInput()
-        // ->withErrors(["email"=> "Your provided credentials could not be verified!"]);
+        session()->regenerate();
+        return redirect("/")->with("success","Welcome back!");
     }
 }
