643edfb6e9cff7e1ea8c7656180f804883494971
Julien Pontbriand <nullzeal@pm.me>
2024-05-26 19:54:43 -0400

    Cleared ep.58

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,19 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us17'
+    ]);
+    $response = $mailchimp->lists->addListMember('d670f7f31d', [
+        "email_address" => "julien2@gmail.com",
+        "status" => "subscribed",
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('show');
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -68,6 +68,7 @@ return array(
     'NunoMaduro\\Collision\\' => array($vendorDir . '/nunomaduro/collision/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'Mockery\\' => array($vendorDir . '/mockery/mockery/library/Mockery'),
+    'MailchimpMarketing\\' => array($vendorDir . '/mailchimp/marketing/lib'),
     'League\\MimeTypeDetection\\' => array($vendorDir . '/league/mime-type-detection/src'),
     'League\\Flysystem\\Local\\' => array($vendorDir . '/league/flysystem-local'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
